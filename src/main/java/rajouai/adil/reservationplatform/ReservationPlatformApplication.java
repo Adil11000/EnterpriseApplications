b3f82541ea287129d0e5ca9c0f3d46466e9c2260
Adil11000 <adil.rajouai@student.ehb.be>
2025-01-05 15:21:18 +0100

    Initial Commit

---
+++ src/main/java/rajouai/adil/reservationplatform/ReservationPlatformApplication.java
@@ -0,0 +1,13 @@
+package rajouai.adil.reservationplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservationPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rajouai/adil/reservationplatform/ReservationPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package rajouai.adil.reservationplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
